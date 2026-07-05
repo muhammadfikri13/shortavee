719096c22dbc72714f03e8f40ebb0b20e7af8432
muhammadfikri13 <muhammadfikri.mf457@gmail.com>
2026-07-05 19:57:10 +0700

    ubah db host pakai var

--- backend/pkg/database/postgres.go
+++ backend/pkg/database/postgres.go
@@ -1,13 +1,22 @@
 package database
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Connect() (*gorm.DB, error) {
+	// 1. Ambil DATABASE_URL dari environment variable (untuk di Render)
+	dsn := os.Getenv("DB_HOST")
 
-	dsn := "host=localhost user=shortavee password=tinyavee123 dbname=shortavee_db port=5432 sslmode=disable"
+	// 2. Jika DATABASE_URL kosong (berarti Anda sedang jalankan di komputer lokal)
+	//    Maka gunakan DSN localhost lama Anda sebagai cadangan (fallback)
+	if dsn == "" {
+		dsn = "host=localhost user=shortavee password=tinyavee123 dbname=shortavee_db port=5432 sslmode=disable"
+	}
 
+	// 3. GORM otomatis mengenali apakah dsn berupa format URL postgres:// atau format host=localhost
 	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
